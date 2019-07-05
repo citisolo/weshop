0c30440e6a28193cb3a71d365aedf57b6c3eb770
Kwadwo Amankwa <citisolo@gmail.com>
2019-07-05 13:07:58 +0100

    refactored product page to use flow format

--- src/views/Product.js
+++ src/views/Product.js
@@ -2,7 +2,34 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchProduct } from '../actions/actions';
 import FlexList from '../components/FlexList';
-import { Image, Grid, Button } from 'semantic-ui-react';
+import { Image, Grid, Button, Header, Label } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const ProductWrapper = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	flex-wrap: wrap;
+	div {
+		margin: 0.2em;
+	}
+`;
+
+const ProductDataWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+
+	h3 {
+		align-self: flex-end;
+		.ui.label {
+			font-size: large;
+		}
+	}
+	button {
+		width: 20%;
+		align-self: flex-end;
+	}
+`;
 
 const Product = props => {
 	const {
@@ -21,21 +48,16 @@ const Product = props => {
 	}, []);
 
 	return (
-		<Grid>
-			<Grid.Row>
-				<Grid.Column width={3}>
-					<Image src={`/${product.ProductPicUrl}`} bordered size="medium" />
-				</Grid.Column>
-				<Grid.Column width={13}>{product.Name}</Grid.Column>
-			</Grid.Row>
-			<Grid.Row>
-				<Grid.Column>
-					{product.Description}
-					{product.Price}
-					<Button>Add to basket</Button>
-				</Grid.Column>
-			</Grid.Row>
-		</Grid>
+		<ProductWrapper>
+			<Image src={`/${product.ProductPicUrl}`} bordered size="medium" />
+			<ProductDataWrapper>
+				<Header as="h2">{product.Description}</Header>
+				<Header as="h3">
+					<Label>{`£${product.Price}`}</Label>
+				</Header>
+				<Button>Add to basket</Button>
+			</ProductDataWrapper>
+		</ProductWrapper>
 	);
 };
 
